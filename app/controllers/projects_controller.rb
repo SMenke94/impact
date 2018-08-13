9887de3bbefe3b6aa871d2b7097bd8f541a66f18
Saskia Menke <menkesaskia@web.de>
2018-08-13 15:13:08 +0200

    added controller methods, edited application controller, added devise:views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,49 @@
 class ProjectsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [ :index, :show ]
+  before_action :find_project, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @projects = Project.all
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :location, :category, :capacity, :photo)
   end
 end
