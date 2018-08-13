2411b7a21c62d9b47198222f24a6e0d8120b87e3
Constance Chapman <constancealicechapman224@gmail.com>
2018-08-13 18:04:01 +0200

    new booking form works but does not redirect anywhere

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @project = Project.find(params[:project_id])
+    @booking.project = @project
     @booking.save
     # figure out where to redirect to
   end
